22e3e84aa984ae422d7019139fd1746f490face7
Jeff Sember <jpsember@gmail.com>
2024-03-24 12:19:13 -0700

    Issue #0

--- src/main/java/bk/BkOper.java
+++ src/main/java/bk/BkOper.java
@@ -2,6 +2,7 @@ package bk;
 
 import static js.base.Tools.*;
 
+import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
 import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 import com.googlecode.lanterna.screen.Screen;
@@ -13,6 +14,7 @@ import bk.gen.BkConfig;
 import js.app.AppOper;
 import js.base.BasePrinter;
 import js.base.DateTimeTools;
+import js.file.Files;
 
 public class BkOper extends AppOper {
 
@@ -49,29 +51,71 @@ public class BkOper extends AppOper {
 
   @Override
   public void perform() {
-    Screen screen = null;
-    Terminal term = null;
+    if (false) {
+      Screen screen = null;
+      Terminal term = null;
+      try {
+        term = new DefaultTerminalFactory().createTerminal();
+        screen = new TerminalScreen(term);
+        WindowBasedTextGUI gui = new MultiWindowTextGUI(screen);
+        screen.startScreen();
+      } catch (Throwable t) {
+        throw asRuntimeException(t);
+      }
+
+      // use GUI here until the GUI wants to exit
+
+      for (int i = 0; i < 20; i++) {
+        pr("i:", i);
+        DateTimeTools.sleepForRealMs(1000);
+      }
+
+      try {
+        screen.stopScreen();
+      } catch (Throwable t) {
+        throw asRuntimeException(t);
+      }
+    }
+
+    DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+    Terminal terminal = null;
     try {
-      term = new DefaultTerminalFactory().createTerminal();
-      screen = new TerminalScreen(term);
-      WindowBasedTextGUI gui = new MultiWindowTextGUI(screen);
-      screen.startScreen();
+      terminal = defaultTerminalFactory.createTerminal();
+      terminal.putString("Hello\n");
+      terminal.flush();
+
+      terminal.enableSGR(SGR.REVERSE);
+      terminal.putString("SGR.REVERSE\n");
+      terminal.resetColorAndSGR();
+      
+      terminal.enableSGR(SGR.BOLD);
+      terminal.putString("SGR.BOLD\n");
+      terminal.resetColorAndSGR();
+      
+      terminal.enableSGR(SGR.BORDERED);
+      terminal.putString("SGR.BORDERED\n");
+      terminal.resetColorAndSGR();
+      
+      terminal.enableSGR(SGR.ITALIC);
+      terminal.putString("SGR.ITALIC\n");
+      terminal.resetColorAndSGR();
+      
+      terminal.flush();
+
+      terminal.resetColorAndSGR();
+      terminal.setCursorPosition(terminal.getCursorPosition().withColumn(0).withRelativeRow(1));
+      terminal.putCharacter('D');
+      terminal.putCharacter('o');
+      terminal.putCharacter('n');
+      terminal.putCharacter('e');
+      terminal.putCharacter('\n');
+      terminal.flush();
+
+      Thread.sleep(2000);
     } catch (Throwable t) {
       throw asRuntimeException(t);
     }
-
-    // use GUI here until the GUI wants to exit
-
-    for (int i = 0; i < 20; i++) {
-      pr("i:", i);
-      DateTimeTools.sleepForRealMs(1000);
-    }
-
-    try {
-      screen.stopScreen();
-    } catch (Throwable t) {
-      throw asRuntimeException(t);
-    }
+    Files.close(terminal);
   }
 
 }
