7dafa4d9b3ec1129442e39158b18d0eb960452ce
Jeff Sember <jpsember@gmail.com>
2024-04-16 09:26:02 -0700

    Issue #55

--- src/test/java/bk/OpenPDFTest.java
+++ src/test/java/bk/OpenPDFTest.java
@@ -1,12 +1,12 @@
 package bk;
 
-import static bk.Util.*;
 import static js.base.Tools.*;
 
 import java.awt.Color;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 import org.junit.Test;
 
@@ -15,6 +15,7 @@ import com.lowagie.text.Font;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 
+import js.base.DateTimeTools;
 import js.file.Files;
 import js.testutil.MyTestCase;
 
@@ -22,45 +23,52 @@ public class OpenPDFTest extends MyTestCase {
 
   @Test
   public void a() {
-
-    var sb = new StringBuilder();
-    for (int line = 0; line < 300; line++) {
-      int maxLen = 110;
-      int len = line % (maxLen - 6);
-      sb.append(String.format("%4d: ", len));
-      for (var i = 0; i < len; i++)
-        sb.append((char) ((i % 26) + 'a'));
-      sb.append('\n');
-    }
-    var text = sb.toString();
-    //    var text = randomText(1000, true);
-    log(text);
-    Document document = new Document();
     try {
+      var sb = new StringBuilder();
+      sb.append(name() + ": " + DateTimeTools.humanTimeString() + "\n\n");
+      for (int line = 0; line < 300; line++) {
+        int maxLen = 110;
+        int len = line % (maxLen - 6);
+        sb.append(String.format("%4d: ", len));
+        for (var i = 0; i < len; i++)
+          sb.append((char) ((i % 26) + 'a'));
+        sb.append('\n');
+      }
+
+      var text = sb.toString();
+      log(text);
+      Document document = new Document();
+
       File target = Files.getDesktopFile("experiment.pdf");
-      //var os = new ByteArrayOutputStream();
       Files.S.deletePeacefully(target);
-      PdfWriter.getInstance(document, new FileOutputStream(target));
+      boolean TOFILE = false;
+      OutputStream bo;
+      ByteArrayOutputStream bs = null;
+      if (TOFILE)
+        bo = new FileOutputStream(target);
+      else {
+        bs = new ByteArrayOutputStream();
+        bo = bs;
+      }
+
+      PdfWriter.getInstance(document, bo);
       document.open();
-      // font and color settings
-      Font font = new Font(Font.COURIER, 12, Font.NORMAL, Color.BLACK);
+      Font font = new Font(Font.COURIER, 9, Font.NORMAL, Color.BLACK);
+
       for (String line : split(text, '\n')) {
         log(line);
         var para = new Paragraph(line, font);
-        log("adding:", para);
         document.add(para);
       }
-      //      var s = new String(os.toByteArray());
 
-      //      log(s);
-      //      Files.S.writeString(, s);
-      //      assertMessage(s);
+      document.close();
 
+      if (!TOFILE) {
+        Files.S.write(bs.toByteArray(), target);
+      }
     } catch (Throwable t) {
       throw asRuntimeException(t);
     }
-    document.close();
-
   }
 
 }
